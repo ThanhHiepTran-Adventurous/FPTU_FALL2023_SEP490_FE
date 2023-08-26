dfb76ce4c7e2c9c42c6e7d8fb007fb12e90469fe
jdev2807 <jdev7k48kc@gmail.com>
2023-08-26 14:14:51 +0700

    change all vue2 syntax to vue3 syntax at managerallpage.vue,prodcuct.v

--- src/views/buyer/ManagerAllPage.vue
+++ src/views/buyer/ManagerAllPage.vue
@@ -8,33 +8,7 @@
   </div>
 </template>
 
-<script>
-import { ref } from 'vue'; // Import ref from the Composition API
-
+<script setup>
 import { defineAsyncComponent } from 'vue';
-
 const LandingHeader = defineAsyncComponent(() => import('@/components/page-sections/LandingHeader.vue'))
-
-
-export default {
-  name: 'ManageAllPage',
-  components: {
-    LandingHeader,
-  },
-  setup() {
-    // Use the setup function for data and methods
-    const data = ref(); // Initialize your data using ref()
-
-    const methods = {};
-
-    return {
-      data,
-      ...methods,
-    };
-  },
-};
 </script>
-
-<style>
-  /* @import <--- import cac css buyer page */
-</style>
