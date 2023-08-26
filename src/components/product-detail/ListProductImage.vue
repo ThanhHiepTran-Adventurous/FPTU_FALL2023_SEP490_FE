5e59f3918f9b5cc8ac94efb453036e499cb34f38
jdev2807 <jdev7k48kc@gmail.com>
2023-08-26 21:59:33 +0700

    [hungq] help in ListProductImage.vue

--- src/components/product-detail/ListProductImage.vue
+++ src/components/product-detail/ListProductImage.vue
@@ -20,13 +20,17 @@
       <div>
         <button class="tt-btn-zomm tt-top-right"><i class="icon-f-86"></i></button>
         <img class="zoom-product" :src="images[0]" :data-zoom-image="images[0]" alt="">
+        <!-- fix me -->
+        {{ images[0] }}  <!-- in thi url van hien ra nhung quang vao source thi k ra  -->
       </div>
     </div>
     <div class="tt-product-single-carousel-vertical">
-      <ul id="smallGallery" class="tt-slick-button-vertical  slick-animated-show-js-custom">
+      <ul id="smallGallery" class="slick-animated-show-js-custom">
         <li v-for="(image, index) in images" :key="index">
           <a href="#" :data-image="image" :data-zoom-image="image">
-            <img :src="image" alt="">
+            <img :src="image" alt=""> 
+            <!-- fix me  -->
+            {{ image }}  <!-- in thi url van hien ra nhung quang vao source thi k ra  -->
           </a>
         </li>
       </ul>
@@ -39,4 +43,5 @@
   opacity: 1;
   transition: opacity 0.2s linear;
 }
+
 </style>
