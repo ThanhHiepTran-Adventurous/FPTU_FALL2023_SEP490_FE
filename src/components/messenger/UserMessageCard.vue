f5eedddcbe58c0ab3b581a34ab504b6229410732
jexk2807 <jdev7k48kc@gmail.com>
2023-09-28 18:26:49 +0700

    [hungq] new chat page

---
+++ src/components/messenger/UserMessageCard.vue
@@ -0,0 +1,16 @@
+<template>
+ <div class="flex flex-col items-center bg-indigo-100 border border-gray-200  w-full py-6 px-4 rounded-lg">
+    <div class="h-20 w-20 rounded-full border overflow-hidden">
+      <img src="https://avatars3.githubusercontent.com/u/2763884?s=128" alt="Avatar" class="h-full w-full" />
+    </div>
+    <div class="text-sm font-semibold mt-2">Aminos Co.</div>
+    <div class="text-xs text-gray-500">Người mua sản phẩm ABC</div>
+    <div class="flex flex-row items-center mt-3">
+      <div class="flex flex-col justify-center h-4 w-8  bg-bb-green rounded-full">
+        <div class="h-3 w-3 bg-white rounded-full self-end mr-1"></div>
+      </div>
+    </div>
+    </div>
+</template>
+<script setup>
+</script>
