d5bc419330670b16d8a293cb42c4f99a0ca975ef
DESKTOP-C3R7I4O\vnxa0 <vnxa01@gmail.com>
2023-10-04 21:36:21 +0700

    page-admin

--- src/common/urlConstant.js
+++ src/common/urlConstant.js
@@ -1,31 +1,38 @@
-const SERVICE_URL = import.meta.env.VITE_SERVER_URL;
+const SERVICE_URL = import.meta.env.VITE_SERVER_URL
 
 export default {
-    base: SERVICE_URL,
-    endpoint: {
-        auth: {
-            register: '/users/register',
-            login: '/public/auth/login',
-            logout: '/auth/logout',
-            verifyOtp: '/users/register/otp/verify',
-            resendOtp: '/users/register/otp/resend',
-            refreshToken: '/auth/access-tokens/refresh',
-            saveFcmToken: '/notification/fcmToken',
-        },
-        user: {
-            info: '/users/access-token/information',
-            getCitizenCard: '/citizenCard/imagesCitizenCard',
-            verifyCitizenCard: '/citizenCard/verify-citizen-card',
-            updateInfo: 'profiles/userProfile',
-            updateAvatar: 'profiles/userProfile/image',
-            resendEmailOtp: 'profiles/register/otp/resend',
-            changeEmail: 'profiles/userProfile/changeEmail',
-            verifyEmailOtp: 'profiles/userProfile/verifyOtpChangeEmail'
-        },
-        address: {
-            allWardsOfDistrict: '/address/districts/{code}',
-            allProvinces: '/address/provinces',
-            allDistrictsOfProvince: '/address/provinces/{code}'
-        }
-    }
+  base: SERVICE_URL,
+  endpoint: {
+    auth: {
+      register: '/users/register',
+      login: '/public/auth/login',
+      logout: '/auth/logout',
+      verifyOtp: '/users/register/otp/verify',
+      resendOtp: '/users/register/otp/resend',
+      refreshToken: '/auth/access-tokens/refresh',
+      saveFcmToken: '/notification/fcmToken',
+    },
+    user: {
+      info: '/users/access-token/information',
+      getAll: '/users/listUser',
+      getCitizenCard: '/citizenCard/imagesCitizenCard',
+      verifyCitizenCard: '/citizenCard/verify-citizen-card',
+      updateInfo: 'profiles/userProfile',
+      updateAvatar: 'profiles/userProfile/image',
+      resendEmailOtp: 'profiles/register/otp/resend',
+      changeEmail: 'profiles/userProfile/changeEmail',
+      verifyEmailOtp: 'profiles/userProfile/verifyOtpChangeEmail',
+    },
+    address: {
+      allWardsOfDistrict: '/address/districts/{code}',
+      allProvinces: '/address/provinces',
+      allDistrictsOfProvince: '/address/provinces/{code}',
+    },
+    brand: {
+      getAll: '/brands/listBrand',
+      deleteById: '/brands/{brandId}',
+      updateById: '/brands/{brandId}',
+      create: '/brands',
+    },
+  },
 }
---
+++ src/views/admin/common/AdminHeader.vue
@@ -0,0 +1,216 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite()
+})
+</script>
+<template>
+  <div class="antialiased bg-gray-50 dark:bg-gray-900">
+    <nav
+      class="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
+      <div class="flex flex-wrap justify-between items-center">
+        <div class="flex justify-start items-center">
+          <button
+            data-drawer-target="drawer-navigation"
+            data-drawer-toggle="drawer-navigation"
+            aria-controls="drawer-navigation"
+            class="p-2 mr-2 text-gray-600 rounded-lg cursor-pointer md:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            <svg
+              aria-hidden="true"
+              class="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              class="hidden w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"></path>
+            </svg>
+            <span class="sr-only">Toggle sidebar</span>
+          </button>
+          <a href="https://flowbite.com" class="flex items-center justify-between mr-4">
+            <img src="https://flowbite.s3.amazonaws.com/logo.svg" class="mr-3 h-8" alt="Flowbite Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Admin</span>
+          </a>
+        </div>
+        <div class="flex items-center lg:order-2">
+          <button
+            type="button"
+            class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            id="user-menu-button"
+            aria-expanded="false"
+            data-dropdown-toggle="dropdown">
+            <span class="sr-only">Open user menu</span>
+            <img
+              class="w-8 h-8 rounded-full"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gough.png"
+              alt="user photo" />
+          </button>
+          <!-- Dropdown menu -->
+          <div
+            class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
+            id="dropdown">
+            <div class="py-3 px-4">
+              <span class="block text-sm font-semibold text-gray-900 dark:text-white">Admin</span>
+              <span class="block text-sm text-gray-900 truncate dark:text-white">name@flowbite.com</span>
+            </div>
+            <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
+              <!-- <li>
+                <a
+                  href="#"
+                  class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+                  >My profile</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+                  >Account settings</a
+                >
+              </li> -->
+            </ul>
+
+            <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Sign out</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Sidebar -->
+
+    <aside
+      class="fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+      aria-label="Sidenav"
+      id="drawer-navigation">
+      <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800">
+        <form action="#" method="GET" class="md:hidden mb-2">
+          <label for="sidebar-search" class="sr-only">Search</label>
+          <div class="relative">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg
+                class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path>
+              </svg>
+            </div>
+            <input
+              type="text"
+              name="search"
+              id="sidebar-search"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              placeholder="Search" />
+          </div>
+        </form>
+        <ul class="space-y-2">
+          <!-- <li>
+            <a
+              href="#"
+              class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+              <svg
+                aria-hidden="true"
+                class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+              </svg>
+              <span class="ml-3">Dashboard</span>
+            </a>
+          </li> -->
+          <li>
+            <RouterLink
+              to="/admin/manage-users"
+              class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 18">
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z" />
+              </svg>
+              <span class="flex-1 ml-3 text-left whitespace-nowrap">Users</span>
+            </RouterLink>
+          </li>
+          <li>
+            <RouterLink
+              to="/admin/manage-brands"
+              class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 19 18">
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13.583 5.445h.01M8.86 16.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 10.31 1l5.734.007A1.968 1.968 0 0 1 18 2.983v5.5a.994.994 0 0 1-.316.727l-7.439 7.5a.975.975 0 0 1-1.385.001Z" />
+              </svg>
+              <span class="flex-1 ml-3 text-left whitespace-nowrap">Brands</span>
+            </RouterLink>
+          </li>
+
+          <li>
+            <RouterLink
+              to="/admin/manage-categories"
+              class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+              <svg
+                class="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20">
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M8 3h4M8 17h4m-9-5V8m14 4V8M1 1h4v4H1V1Zm14 0h4v4h-4V1ZM1 15h4v4H1v-4Zm14 0h4v4h-4v-4Z" />
+              </svg>
+              <span class="flex-1 ml-3 text-left whitespace-nowrap">Categories</span>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  </div>
+</template>
