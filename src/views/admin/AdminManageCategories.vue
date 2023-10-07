9fdb025fb674e2d5453e389bb12d093da1ddb45d
DESKTOP-C3R7I4O\vnxa0 <vnxa01@gmail.com>
2023-10-07 16:32:31 +0700

    display-data-category

--- src/views/admin/AdminManageCategories.vue
+++ src/views/admin/AdminManageCategories.vue
@@ -3,8 +3,9 @@ import AdminHeader from '@/views/admin/common/AdminHeader.vue'
 import loginService from '../../services/login.service'
 import { onMounted, ref, computed, watch } from 'vue'
 import VueDatePicker from '@vuepic/vue-datepicker'
+import adminService from '../../services/admin.service'
 import '@vuepic/vue-datepicker/dist/main.css'
-const userList = ref([])
+const categoriesList = ref([])
 const searchQuery = ref('')
 const openDropdowns = ref([])
 const itemsPerPage = 4
@@ -12,7 +13,7 @@ const currentPage = ref(1)
 const showUpdateModal = ref(false)
 const date = ref(new Date())
 onMounted(() => {
-  getAllUsers()
+  getAllCategories()
 })
 const selectedUser = ref(null)
 const openUserModal = user => {
@@ -20,10 +21,11 @@ const openUserModal = user => {
   // console.log(selectedUser.value)
   showUpdateModal.value = true // Show the modal
 }
-const getAllUsers = async () => {
+const getAllCategories = async () => {
   try {
-    const response = await loginService.fetchAllUserInfo()
-    userList.value = response.data
+    const response = await adminService.getAllCategories()
+    categoriesList.value = response.data
+    console.log(categoriesList.value)
   } catch (e) {
     console.error(e)
   }
@@ -45,7 +47,7 @@ const toggleDropdown = index => {
 const paginatedUsers = computed(() => {
   const startIndex = (currentPage.value - 1) * itemsPerPage
   const endIndex = startIndex + itemsPerPage
-  return userList.value
+  return categoriesList.value
     .filter(user => user.fullname.toLowerCase().includes(searchQuery.value.toLowerCase()))
     .slice(startIndex, endIndex)
 })
@@ -57,10 +59,7 @@ watch(searchQuery, () => {
 
 // Computed property for total pages
 const totalPages = computed(() => {
-  return Math.ceil(
-    userList.value.filter(user => user.fullname.toLowerCase().includes(searchQuery.value.toLowerCase())).length /
-      itemsPerPage,
-  )
+  return Math.ceil(categoriesList.value.length / itemsPerPage)
 })
 // Function to go to a specific page
 const goToPage = page => {
@@ -80,7 +79,140 @@ const goToNextPage = () => {
     currentPage.value += 1
   }
 }
+// Compute the start and end index for the current page
+const startIndex = (currentPage.value - 1) * itemsPerPage
+const endIndex = startIndex + itemsPerPage
+
+// Compute paginated auctions
+const paginateCategories = computed(() => {
+  // Move startIndex and endIndex calculation here
+  const startIndex = (currentPage.value - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  return categoriesList.value.slice(startIndex, endIndex)
+})
 </script>
 <template>
   <AdminHeader />
+  <section class="bg-white ml-20 mt-5 p-3 sm:p-5">
+    <div class="mx-auto max-w-screen-lg pl-5 px-4 lg:px-12">
+      <!-- Start coding here -->
+      <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+        <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+          <div class="w-full md:w-1/2"></div>
+          <div
+            class="flex flex-col flex-shrink-0 space-y-3 md:flex-row md:items-center lg:justify-end md:space-y-0 md:space-x-3"></div>
+        </div>
+        <div class="overflow-x-auto">
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-5 py-3">Tên</th>
+
+                <th scope="col" class="px-4 py-3">Mô tả</th>
+                <th scope="col" class="px-4 py-3">Trạng thái</th>
+
+                <th scope="col" class="px-4 py-3">
+                  <span class="sr-only">Actions</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(category, index) in paginateCategories" :key="index" class="border-b dark:border-gray-700">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                  <!-- <img class="w-10 h-10 rounded-full" :src="category.imageUrl" :alt="category.name + ' image'" /> -->
+                  <div class="pl-3">
+                    <div class="text-base font-semibold">{{ category.name }}</div>
+                  </div>
+                </th>
+                <td class="px-4 py-3">{{ category.description }}</td>
+                <td class="px-4 py-3">{{ category.status }}</td>
+                <td class="px-4 py-3 flex items-center justify-end">
+                  <button
+                    class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
+                    type="button"
+                    @click="openAuctionModal(auction)">
+                    <svg
+                      class="w-6 h-6 text-gray-800 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 18">
+                      <path
+                        d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z" />
+                      <path
+                        d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <nav
+          class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+          aria-label="Table navigation">
+          <ul class="inline-flex items-stretch -space-x-px">
+            <li>
+              <button
+                type="button"
+                class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                @click="goToPreviousPage"
+                :disabled="currentPage === 1"
+                aria-label="Previous Page">
+                <span class="sr-only">Previous</span>
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                    clip-rule="evenodd" />
+                </svg>
+              </button>
+            </li>
+            <!-- Generate pagination links -->
+            <li v-for="pageNumber in totalPages" :key="pageNumber">
+              <button
+                type="button"
+                class="flex items-center justify-center text-sm py-2 px-3 leading-tight"
+                :class="{
+                  'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white':
+                    pageNumber !== currentPage,
+                  'text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white':
+                    pageNumber === currentPage,
+                }"
+                @click="goToPage(pageNumber)"
+                aria-label="Page {{ pageNumber }}">
+                {{ pageNumber }}
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                @click="goToNextPage"
+                class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                :disabled="currentPage === totalPages"
+                aria-label="Next Page">
+                <span class="sr-only">Next</span>
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewbox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd" />
+                </svg>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </section>
 </template>
