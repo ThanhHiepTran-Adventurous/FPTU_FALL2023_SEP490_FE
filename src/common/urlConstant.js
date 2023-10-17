92c9a3f34d7f3029ed7729b1d59c8a0fa16a1bc5
DESKTOP-A8V6GUM\vnxa01 <vnxa01@gmail.com>
2023-10-17 14:02:11 +0700

    add filter auctions

--- src/common/urlConstant.js
+++ src/common/urlConstant.js
@@ -1,68 +1,70 @@
 const SERVICE_URL = import.meta.env.VITE_SERVER_URL
 
 export default {
-    base: SERVICE_URL,
-    endpoint: {
-        auth: {
-            register: '/users/register',
-            login: '/public/auth/login',
-            logout: '/auth/logout',
-            verifyOtp: '/users/register/otp/verify',
-            resendOtp: '/users/register/otp/resend',
-            refreshToken: '/auth/access-tokens/refresh',
-            saveFcmToken: '/notification/fcmToken',
-        },
-        user: {
-            info: '/users/access-token/information',
-            getAll: '/users/listUser',
-            getCitizenCard: '/citizenCard/imagesCitizenCard',
-            verifyCitizenCard: '/citizenCard/verify-citizen-card',
-            updateInfo: 'profiles/userProfile',
-            updateAvatar: 'profiles/userProfile/image',
-            resendEmailOtp: 'profiles/register/otp/resend',
-            changeEmail: 'profiles/userProfile/changeEmail',
-            verifyEmailOtp: 'profiles/userProfile/verifyOtpChangeEmail'
-        },
-        address: {
-            allWardsOfDistrict: '/address/districts/{code}',
-            allProvinces: '/address/provinces',
-            allDistrictsOfProvince: '/address/provinces/{code}'
-        },
-        brand: {
-          getAll: '/brands/listBrand',
-          deleteById: '/brands/{brandId}',
-          updateById: '/brands/{brandId}',
-          create: '/brands',
-        },
-        category: {
-            getAll: '/categories/listCategory'
-        },
-        product: {
-            inventory: '/products',
-            inventoryByUser: '/products/listProductBySeller'
-        },
-        auctions: {
-          getAll: '/auctions',
-          getDetail: '/auctions/public/{id}/auction-detail',
-          productAuctionRequest: '/auctions/{productId}/add-auction-product',
-          approve: '/auctions/auction-sessions/{auctionId}/approve',
-          reject: '/auctions/auction-sessions/{auctionId}/rejected',
-          getHistoryBid: '/auctions/public/auction/{auctionId}',
-          autoAuctionDetail: '/auctions/{auctionId}/auctionAutoInfo-detail',
-          updateAutoAuction: '/auctions/buyer/edit-auto-auction-info/{autoAuctionId}',
-          getListAuctionWin: '/auctions/getListAuctionWin',
-        },
-        placeBid: {
-            manual: 'auctions/buyer/manual-auction',
-            auto: 'auctions/buyer/add-auto-auction-info/{auctionId}',
-            buyNow: 'auctions/{auctionId}/direct-purchase-auction',
-        },
-        guest: {
-            auctions: 'guest/auctions',
-            auctionDetail: 'guest/{id}/auction-detail',
-            auctionHistory: 'guest/auction/{auctionId}'
-        }
-    }
+  base: SERVICE_URL,
+  endpoint: {
+    auth: {
+      register: '/users/register',
+      login: '/public/auth/login',
+      logout: '/auth/logout',
+      verifyOtp: '/users/register/otp/verify',
+      resendOtp: '/users/register/otp/resend',
+      refreshToken: '/auth/access-tokens/refresh',
+      saveFcmToken: '/notification/fcmToken',
+    },
+    user: {
+      info: '/users/access-token/information',
+      getAll: '/users/listUser',
+      getCitizenCard: '/citizenCard/imagesCitizenCard',
+      verifyCitizenCard: '/citizenCard/verify-citizen-card',
+      updateInfo: 'profiles/userProfile',
+      updateAvatar: 'profiles/userProfile/image',
+      resendEmailOtp: 'profiles/register/otp/resend',
+      changeEmail: 'profiles/userProfile/changeEmail',
+      verifyEmailOtp: 'profiles/userProfile/verifyOtpChangeEmail',
+    },
+    address: {
+      allWardsOfDistrict: '/address/districts/{code}',
+      allProvinces: '/address/provinces',
+      allDistrictsOfProvince: '/address/provinces/{code}',
+    },
+    brand: {
+      getAll: '/brands/listBrand',
+      deleteById: '/brands/{brandId}',
+      updateById: '/brands/{brandId}',
+      create: '/brands',
+    },
+    category: {
+      getAll: '/categories/listCategory',
+    },
+    product: {
+      inventory: '/products',
+      inventoryByUser: '/products/listProductBySeller',
+      updateProductById: 'products/{productId}',
+    },
+    auctions: {
+      getAll: '/auctions',
+      getDetail: '/auctions/public/{id}/auction-detail',
+      productAuctionRequest: '/auctions/{productId}/add-auction-product',
+      approve: '/auctions/auction-sessions/{auctionId}/approve',
+      reject: '/auctions/auction-sessions/{auctionId}/rejected',
+      getHistoryBid: '/auctions/public/auction/{auctionId}',
+      autoAuctionDetail: '/auctions/{auctionId}/auctionAutoInfo-detail',
+      updateAutoAuction: '/auctions/buyer/edit-auto-auction-info/{autoAuctionId}',
+      getListAuctionWin: '/auctions/getListAuctionWin',
+    },
+    placeBid: {
+      manual: 'auctions/buyer/manual-auction',
+      auto: 'auctions/buyer/add-auto-auction-info/{auctionId}',
+      buyNow: 'auctions/{auctionId}/direct-purchase-auction',
+    },
+    guest: {
+      auctions: 'guest/auctions',
+      auctionDetail: 'guest/{id}/auction-detail',
+      auctionHistory: 'guest/auction/{auctionId}',
+    },
+  },
 }
 
-export const noImage = "https://firebasestorage.googleapis.com/v0/b/bidbay-project.appspot.com/o/No-Image-Placeholder.svg.png?alt=media"
+export const noImage =
+  'https://firebasestorage.googleapis.com/v0/b/bidbay-project.appspot.com/o/No-Image-Placeholder.svg.png?alt=media'
--- src/views/buyer/child-page/AuctionListPage.vue
+++ src/views/buyer/child-page/AuctionListPage.vue
@@ -1,44 +1,44 @@
 <script setup>
-import Dropdown from '@/components/common-components/Dropdown.vue';
-import ItemBox from '@/components/common-components/item-box/ItemBox.vue';
-import { onMounted, ref } from 'vue';
-import { Icon } from '@iconify/vue';
+import Dropdown from '@/components/common-components/Dropdown.vue'
+import ItemBox from '@/components/common-components/item-box/ItemBox.vue'
+import { onMounted, ref, computed } from 'vue'
+import { Icon } from '@iconify/vue'
 import Slider from '@vueform/slider'
 import currencyFormat from '@/utils/currency-formatter.js'
-import brandService from '@/services/brand.service';
-import categoryService from '@/services/category.service';
-import auctionService from '@/services/auction.service';
-import imageHelper from '@/utils/image-helper';
-
+import brandService from '@/services/brand.service'
+import categoryService from '@/services/category.service'
+import auctionService from '@/services/auction.service'
+import imageHelper from '@/utils/image-helper'
+import { Pagination } from 'flowbite-vue'
 const orderOptions = ref([
-    {
-        label: "Số lượng lượt đấu giá tăng dần",
-        data: "d1",
-    },
-    {
-        label: "Số lượng lượt đấu giá giảm dần",
-        data: "d2",
-    },
-    {
-        label: "Thời gian đấu giá còn lại dài nhất",
-        data: "d3",
-    },
-    {
-        label: "Thời gian đấu giá còn lại ngắn nhất",
-        data: "d4",
-    },
-    {
-        label: "Giá tăng dần",
-        data: "d5",
-    },
-    {
-        label: "Giá giảm dần",
-        data: "d6",
-    },
+  {
+    label: 'Số lượng lượt đấu giá tăng dần',
+    data: 'd1',
+  },
+  {
+    label: 'Số lượng lượt đấu giá giảm dần',
+    data: 'd2',
+  },
+  {
+    label: 'Thời gian đấu giá còn lại dài nhất',
+    data: 'd3',
+  },
+  {
+    label: 'Thời gian đấu giá còn lại ngắn nhất',
+    data: 'd4',
+  },
+  {
+    label: 'Giá tăng dần',
+    data: 'd5',
+  },
+  {
+    label: 'Giá giảm dần',
+    data: 'd6',
+  },
 ])
 const orderSelected = ref({
-    label: "Số lượng lượt đấu giá tăng dần",
-    data: "d1",
+  label: 'Số lượng lượt đấu giá tăng dần',
+  data: 'd1',
 })
 const priceSelected = ref([100000, 5000000])
 const isBrandTabOpen = ref(false)
@@ -49,119 +49,189 @@ const brandOptions = ref([])
 const categoryOptions = ref([])
 
 const auctions = ref([])
-
+const itemsPerPage = 10
+const currentPage = ref(1)
+const totalAuctions = ref(1)
 const fetchBrandsData = async () => {
-    const brands = await brandService.getAllBrands()
-    brandOptions.value = brands.data.map(d => {
-        return {
-            label: d.name,
-            value: d.id,
-            isSelected: false,
-        }
-    })
+  const brands = await brandService.getAllBrands()
+  brandOptions.value = brands.data.map(d => {
+    return {
+      label: d.name,
+      value: d.id,
+      isSelected: false,
+    }
+  })
 }
 const fetchCategoriesData = async () => {
-    const categories = await categoryService.getAllCategories()
-    
-    categoryOptions.value = await categories.data.map(d => {
-        return {
-            label: d.name,
-            value: d.id,
-            isSelected: false,
-        }
-    })
+  const categories = await categoryService.getAllCategories()
+
+  categoryOptions.value = await categories.data.map(d => {
+    return {
+      label: d.name,
+      value: d.id,
+      isSelected: false,
+    }
+  })
 }
 const fetchAuctions = async () => {
-    const auctionsData = await auctionService.getAllActiveAuctions()
-    auctions.value = auctionsData.data
+  const auctionsData = await auctionService.getAllActiveAuctions()
+  console.log(auctionsData)
+  auctions.value = auctionsData.data
+  totalAuctions.value = auctions.length
 }
 
 onMounted(async () => {
-    fetchBrandsData()
-    fetchCategoriesData()
-    fetchAuctions()
+  fetchBrandsData()
+  fetchCategoriesData()
+  fetchAuctions()
 })
 
+// Create a computed property for paginated auctions
+const paginatedAuctions = computed(() => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  return filteredAuctions.value.slice(startIndex, endIndex)
+})
+
+const filteredAuctions = computed(() => {
+  // Filter by selected brands
+  const selectedBrands = brandOptions.value.filter(brand => brand.isSelected).map(brand => brand.value)
+  const brandFilteredAuctions =
+    selectedBrands.length > 0
+      ? auctions.value.filter(auction => selectedBrands.includes(auction.product.brand.id))
+      : auctions.value
+
+  // Filter by selected categories
+  const selectedCategories = categoryOptions.value
+    .filter(category => category.isSelected)
+    .map(category => category.value)
+  const categoryFilteredAuctions =
+    selectedCategories.length > 0
+      ? auctions.value.filter(auction => selectedCategories.includes(auction.product.category.id))
+      : auctions.value
+
+  // Filter by price
+  const priceFilteredAuctions = auctions.value.filter(auction => {
+    const price = auction.highestPrice || auction.startPrice
+    return price >= priceSelected.value[0] && price <= priceSelected.value[1]
+  })
+
+  // Combine all filters
+  const filteredByBrandAndCategory = brandFilteredAuctions.filter(auction => categoryFilteredAuctions.includes(auction))
+  return filteredByBrandAndCategory.filter(auction => priceFilteredAuctions.includes(auction))
+})
 </script>
 <template>
-    <div class="w-full">
-        <div class="container mx-auto bg-white mt-3 rounded-md px-5 pt-1 overflow-auto">
-            <div class="flex items-center justify-between">
-                <p class="text-3xl font-bold text-black">Sản phẩm</p>
-                <div class="flex items-center gap-3">
-                    <p>Sắp xếp theo: </p>
-                    <div class="mt-3">
-                        <Dropdown v-model="orderSelected" :data="orderOptions" class="!w-[300px]" />
-                    </div>
-                </div>
-            </div>
-            <div class="flex items-start justify-between mt-7">
-                <!-- Filter section -->
-                <div>
-                    <div>Bộ lọc</div>
-                    <div class="mb-1.5 mt-2">
-                        <div class="flex items-center justify-between hover:cursor-pointer"
-                            @click="() => isBrandTabOpen = !isBrandTabOpen">
-                            <div class="font-bold text-black">Thương hiệu</div>
-                            <Icon icon="ep:arrow-down" class="text-[18px]" />
-                        </div>
-                        <div
-                            :class="`transition-all duration-500 ${isBrandTabOpen ? 'h-[10rem] opacity-1 p-2' : 'h-0 opacity-0 overflow-hidden'}`">
-                            <div v-for="brand in brandOptions" :key="brand.id"  class="flex items-center mb-1">
-                                <input :id="brand.id" type="checkbox" :value="brand.value" v-model="brand.isSelected"
-                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
-                                <label for="default-checkbox"
-                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ brand.label }}</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mb-1.5">
-                        <div class="flex items-center justify-between hover:cursor-pointer"
-                            @click="() => isPriceTabOpen = !isPriceTabOpen">
-                            <div class="font-bold text-black">Giá</div>
-                            <Icon icon="ep:arrow-down" class="text-[18px]" />
-                        </div>
-                        <div
-                            :class="`transition-all duration-500 ${isPriceTabOpen ? 'h-[10rem] opacity-1 p-2' : 'h-0 opacity-0 overflow-hidden'}`">
-                            <div class="mt-5">
-                                <Slider v-model="priceSelected" :max="10000000" :step="10000" />
-                                <p>Giá từ {{ currencyFormat(priceSelected[0]) }} đên {{ currencyFormat(priceSelected[1]) }}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mb-1.5">
-                        <div class="flex items-center justify-between hover:cursor-pointer"
-                            @click="() => isCategoryTabOpen = !isCategoryTabOpen">
-                            <div class="font-bold text-black">Chủng loại</div>
-                            <Icon icon="ep:arrow-down" class="text-[18px]" />
-                        </div>
-                        <div
-                            :class="`transition-all duration-500 ${isCategoryTabOpen ? 'h-[10rem] opacity-1 p-2' : 'h-0 opacity-0 overflow-hidden'}`">
-                            <div v-for="cate in categoryOptions" :key="cate.id"  class="flex items-center mb-1">
-                                <input :id="cate.id" type="checkbox" :value="cate.value" v-model="cate.isSelected"
-                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
-                                <label for="default-checkbox"
-                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ cate.label }}</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="p-2 mt-8 ml-4 bg-blue-600 text-md text-white rounded-lg w-[100px] text-center" @click="onFilter()">Lọc</div>
-                </div>
-                <!-- Auction section -->
-                <div class="w-[75%]">
-                <div class="flex flex-wrap gap-8">
-                    <ItemBox v-for="auction in auctions" :key="auction.id"
-                        :product-name="auction.product.name"
-                        :main-image="imageHelper.getPrimaryImageFromList(auction.product.imageUrls)"
-                        :secondary-image="imageHelper.getSecondaryImageFromList(auction.product.imageUrls)"
-                        :floor-price="auction.highestPrice ? auction.highestPrice : auction.startPrice"
-                        :time-remain="auction.timeLeft"
-                        :item-id="auction.id"
-                        />
-                </div>
-            </div>
+  <div class="w-full">
+    <div class="container mx-auto bg-white mt-3 rounded-md px-5 pt-1 overflow-auto">
+      <div class="flex items-center justify-between">
+        <p class="text-3xl font-bold text-black">Sản phẩm</p>
+        <div class="flex items-center gap-3">
+          <p>Sắp xếp theo:</p>
+          <div class="mt-3">
+            <Dropdown v-model="orderSelected" :data="orderOptions" class="!w-[300px]" />
+          </div>
         </div>
+      </div>
+      <div class="flex items-start justify-between mt-7">
+        <!-- Filter section -->
+        <div>
+          <div>Bộ lọc</div>
+          <div class="mb-1.5 mt-2">
+            <div
+              class="flex items-center justify-between hover:cursor-pointer"
+              @click="() => (isBrandTabOpen = !isBrandTabOpen)">
+              <div class="font-bold text-black">Thương hiệu</div>
+              <Icon icon="ep:arrow-down" class="text-[18px]" />
+            </div>
+            <div
+              :class="`transition-all duration-500 ${
+                isBrandTabOpen ? 'h-[10rem] opacity-1 p-2' : 'h-0 opacity-0 overflow-hidden'
+              }`">
+              <div v-for="brand in brandOptions" :key="brand.id" class="flex items-center mb-1">
+                <input
+                  :id="brand.id"
+                  type="checkbox"
+                  :value="brand.value"
+                  v-model="brand.isSelected"
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
+                <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{
+                  brand.label
+                }}</label>
+              </div>
+            </div>
+          </div>
+          <div class="mb-1.5">
+            <div
+              class="flex items-center justify-between hover:cursor-pointer"
+              @click="() => (isPriceTabOpen = !isPriceTabOpen)">
+              <div class="font-bold text-black">Giá</div>
+              <Icon icon="ep:arrow-down" class="text-[18px]" />
+            </div>
+            <div
+              :class="`transition-all duration-500 ${
+                isPriceTabOpen ? 'h-[10rem] opacity-1 p-2' : 'h-0 opacity-0 overflow-hidden'
+              }`">
+              <div class="mt-5">
+                <Slider v-model="priceSelected" :max="10000000" :step="10000" />
+                <p>Giá từ {{ currencyFormat(priceSelected[0]) }} đên {{ currencyFormat(priceSelected[1]) }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="mb-1.5">
+            <div
+              class="flex items-center justify-between hover:cursor-pointer"
+              @click="() => (isCategoryTabOpen = !isCategoryTabOpen)">
+              <div class="font-bold text-black">Chủng loại</div>
+              <Icon icon="ep:arrow-down" class="text-[18px]" />
+            </div>
+            <div
+              :class="`transition-all duration-500 ${
+                isCategoryTabOpen ? 'h-[10rem] opacity-1 p-2' : 'h-0 opacity-0 overflow-hidden'
+              }`">
+              <div v-for="cate in categoryOptions" :key="cate.id" class="flex items-center mb-1">
+                <input
+                  :id="cate.id"
+                  type="checkbox"
+                  :value="cate.value"
+                  v-model="cate.isSelected"
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500" />
+                <label for="default-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{
+                  cate.label
+                }}</label>
+              </div>
+            </div>
+          </div>
+          <!-- <div
+            class="p-2 mt-8 ml-4 bg-blue-600 text-md text-white rounded-lg w-[100px] text-center"
+            @click="onFilter()">
+            Lọc
+          </div> -->
+        </div>
+        <!-- Auction section -->
+        <div class="w-[75%]">
+          <div class="mb-2 mt-2">
+            <Pagination
+              v-model="currentPage"
+              :per-page="10"
+              :total-items="totalAuctions"
+              previous-label="⬅️"
+              next-label="➡️"></Pagination>
+          </div>
+          <div class="flex flex-wrap gap-8">
+            <ItemBox
+              v-for="auction in paginatedAuctions"
+              :key="auction.id"
+              :product-name="auction.product.name"
+              :main-image="imageHelper.getPrimaryImageFromList(auction.product.imageUrls)"
+              :secondary-image="imageHelper.getSecondaryImageFromList(auction.product.imageUrls)"
+              :floor-price="auction.highestPrice ? auction.highestPrice : auction.startPrice"
+              :time-remain="auction.timeLeft"
+              :item-id="auction.id" />
+          </div>
+        </div>
+      </div>
     </div>
-</div></template>
+  </div>
+</template>
 <style src="@vueform/slider/themes/default.css"></style>
