eeb26f470441693952b1abc80f9562ea24651371
Truong Quang Tan <quangtan9bgm@gmail.com>
2023-11-25 00:11:23 +0700

    update banner image

--- src/components/page-sections/landing-page/PosterSection.vue
+++ src/components/page-sections/landing-page/PosterSection.vue
@@ -7,17 +7,17 @@ import 'vue3-carousel/dist/carousel.css'
         <Carousel>
             <Slide key="banner1">
                 <div class="container">
-                    <img src="https://salt.tikicdn.com/ts/brickv2og/c0/f4/20/333a4bf61378b404f0ca015332b85d12.png" alt="banner" class="w-full h-auto"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png" alt="banner" class="w-full h-auto"/>
                 </div>
             </Slide>
             <Slide key="banner2">
                 <div class="container">
-                    <img src="https://shopeeplus.com//upload/images/tiki-freeship.png" alt="banner" class="w-full h-auto"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png" alt="banner" class="w-full h-auto"/>
                 </div>
             </Slide>
             <Slide key="banner3">
                 <div class="container">
-                    <img src="https://thietkehaithanh.com/wp-content/uploads/2019/06/thietkehaithanh-banner-1-4.jpg" alt="banner" class="w-full h-auto"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png" alt="banner" class="w-full h-auto"/>
                 </div>
             </Slide>
             <template #addons>
