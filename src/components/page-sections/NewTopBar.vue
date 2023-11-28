8860e5a19ff236224ee39d22ad8280865288689c
Truong Quang Tan <quangtan9bgm@gmail.com>
2023-11-28 17:22:44 +0700

    link to nav

--- src/components/page-sections/NewTopBar.vue
+++ src/components/page-sections/NewTopBar.vue
@@ -4,10 +4,10 @@ import TopBarItem from '@/components/page-sections/TopBarItem.vue'
 <template>
   <div class="py-2 mx-auto bg-white">
     <div class="flex w-full gap-8 justify-center items-center overflow-auto">
-      <router-link to="/auctions"><TopBarItem text="Áo" icon="shirt" /></router-link>
-      <router-link to="/auctions"><TopBarItem text="Quần" icon="pants" /></router-link>
-      <router-link to="/auctions"><TopBarItem text="Phụ kiện" icon="accessory" /></router-link>
-      <router-link to="/auctions"><TopBarItem text="Giày" icon="shoes" /></router-link>
+      <router-link to="/auctions?category=3974f109-8dce-11ee-871a-8685dbe3cd15"><TopBarItem text="Áo" icon="shirt" /></router-link>
+      <router-link to="/auctions?category=397507e6-8dce-11ee-871a-8685dbe3cd15"><TopBarItem text="Quần" icon="pants" /></router-link>
+      <router-link to="/auctions?category=9de51024-8c73-11ee-871a-8685dbe3cd15"><TopBarItem text="Phụ kiện" icon="accessory" /></router-link>
+      <router-link to="/auctions?category=39750af7-8dce-11ee-871a-8685dbe3cd15"><TopBarItem text="Giày" icon="shoes" /></router-link>
       <router-link to="/auctions"><TopBarItem text="Nổi bật" icon="topSeller" /></router-link>
     </div>
   </div>
