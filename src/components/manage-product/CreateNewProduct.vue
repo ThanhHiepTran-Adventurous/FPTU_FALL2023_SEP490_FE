6f19c637c8606cb6f72956b4fcfd426a1ed6ea41
DESKTOP-A8V6GUM\vnxa01 <vnxa01@gmail.com>
2023-11-29 10:11:55 +0700

    validate form create new product - create new auction

--- src/components/manage-product/CreateNewProduct.vue
+++ src/components/manage-product/CreateNewProduct.vue
@@ -8,7 +8,9 @@ import productSerivice from '@/services/product.service'
 import toastOption from '@/utils/toast-option'
 import { HTTP_STATUS } from '@/common/httpStatus'
 import * as yup from 'yup'
-import { ErrorMessage, Field, Form } from 'vee-validate'
+import { Field, Form } from 'vee-validate'
+import ErrorMessage from '../common-components/ErrorMessage.vue'
+
 import ListImage from '@/components/ListEditableImage.vue'
 
 const emit = defineEmits(['createSuccess', 'createError', 'justSubmitted'])
@@ -34,55 +36,94 @@ const formData = ref({
   brandId: '',
   categoryId: '',
 })
-
+const manualAuctionErrorState = ref({
+  name: '',
+  description: '',
+  weight: '',
+  brand: '',
+  category: '',
+})
+const resetErrorState = () => {
+  manualAuctionErrorState.value = {
+    name: '',
+    description: '',
+    weight: '',
+    brand: '',
+    category: '',
+    image: '',
+  }
+}
+const validateManual = () => {
+  if (formData.value.name.trim() === '') {
+    manualAuctionErrorState.value.name = 'Vui lòng nhập tên sản phẩm'
+    return false
+  }
+  if (formData.value.description.trim() === '') {
+    manualAuctionErrorState.value.description = 'Vui lòng nhập miêu tả'
+    return false
+  }
+  if (!formData.value.weight) {
+    manualAuctionErrorState.value.weight = 'Vui lòng nhập trọng lượng'
+    return false
+  }
+  if (!formData.value.brandId) {
+    console.log(formData.value.brandId)
+    manualAuctionErrorState.value.brand = 'Vui lòng chọn thương hiệu sản phẩm'
+    return false
+  }
+  if (!formData.value.categoryId) {
+    manualAuctionErrorState.value.category = 'Vui lòng chọn loại sản phẩm'
+    return false
+  }
+  if (imgSrc.value.length === 0) {
+    manualAuctionErrorState.value.image = 'Vui lòng tải lên ít nhất một hình ảnh'
+    return false
+  }
+  return true
+}
 const onSubmit = async () => {
-  try {
-    await productSchema.validate(formData.value, { abortEarly: false })
-    validate.value = true
-  } catch (validationErrors) {
-    validate.value = false
+  resetErrorState()
+  if (!validateManual()) {
     return
   }
-  if ((validate.value === true)) {
-    const form = new FormData()
+  const form = new FormData()
 
-    const addProductRequest = {
-      name: formData.value.name,
-      description: formData.value.description,
-      weight: formData.value.weight,
-      brandId: formData.value.brandId,
-      categoryId: formData.value.categoryId,
-    }
-
-    for (const imageData of imgData.value) {
-      form.append('images', imageData)
-    }
-    form.append('addProductRequest', new Blob([JSON.stringify(addProductRequest)], { type: 'application/json' }))
-
-    emit('justSubmitted')
-    const toastId = toastOption.toastLoadingMessage('Đang thêm sản phẩm vào kho')
-
-    productSerivice
-      .addProductToInventory(form)
-      .then(_ => {
-        emit('createSuccess', toastId)
-      })
-      .catch(error => {
-        if (error.response.status === HTTP_STATUS.INTERNAL_ERROR) {
-          emit('createError', toastId)
-          return
-        }
-        if (error.response.data.message.includes('Validation')) {
-          emit('createError', toastId, 'Kiểm tra lại thông tin của bạn')
-        } else if (error.response.data.message.includes('citizen card')) {
-          emit('createError', toastId, 'Bạn phải xác thực căn cước công dân trước khi đăng sản phẩm')
-        }
-        emit('createError', toastId)
-      })
-      .finally(() => {
-        resetFormData()
-      })
+  const addProductRequest = {
+    name: formData.value.name,
+    description: formData.value.description,
+    weight: formData.value.weight,
+    brandId: formData.value.brandId,
+    categoryId: formData.value.categoryId,
   }
+
+  for (const imageData of imgData.value) {
+    form.append('images', imageData)
+  }
+  form.append('addProductRequest', new Blob([JSON.stringify(addProductRequest)], { type: 'application/json' }))
+
+  emit('justSubmitted')
+  const toastId = toastOption.toastLoadingMessage('Đang thêm sản phẩm vào kho')
+
+  productSerivice
+    .addProductToInventory(form)
+    .then(_ => {
+      emit('createSuccess', toastId)
+    })
+    .catch(error => {
+      if (error.response.status === HTTP_STATUS.INTERNAL_ERROR) {
+        emit('createError', toastId)
+        return
+      }
+      if (error.response.data.message.includes('Validation')) {
+        emit('createError', toastId, 'Kiểm tra lại thông tin của bạn')
+      } else if (error.response.data.message.includes('citizen card')) {
+        emit('createError', toastId, 'Bạn phải xác thực căn cước công dân trước khi đăng sản phẩm')
+      }
+      emit('createError', toastId)
+    })
+    .finally(() => {
+      resetFormData()
+    })
 }
 
 const handleFileUpload = async e => {
@@ -90,7 +131,7 @@ const handleFileUpload = async e => {
   imgSrc.value.push(await base64Image(e.target.files[0]))
 }
 
-const handleImageDeleted = (indx) => {
+const handleImageDeleted = indx => {
   imgSrc.value.splice(indx, 1)
   imgData.value.splice(indx, 1)
 }
@@ -117,59 +158,73 @@ const resetFormData = () => {
 <template>
   <form class="bg-white rounded px-8 pb-8 mb-4" @submit.prevent="onSubmit()">
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="title"> Tên Sản Phẩm <span class="text-red-500">*</span> </label>
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
+        Tên Sản Phẩm <span class="text-red-500">*</span>
+      </label>
       <input
         v-model="formData.name"
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="title"
         type="text"
-        required
         placeholder="Nhập tên sản phẩm" />
+      <ErrorMessage :text="manualAuctionErrorState.name" />
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="description"> Mô tả <span class="text-red-500">*</span> </label>
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
+        Mô tả <span class="text-red-500">*</span>
+      </label>
       <textarea
         v-model="formData.description"
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="description"
         rows="4"
-        required
         placeholder="Nhập mô tả"></textarea>
+      <ErrorMessage :text="manualAuctionErrorState.description" />
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="weight"> Trọng lượng (gram) <span class="text-red-500">*</span> </label>
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="weight">
+        Trọng lượng (gram) <span class="text-red-500">*</span>
+      </label>
       <input
         v-model="formData.weight"
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="weight"
-        type="text"
-        required
+        type="number"
+        min="0"
         placeholder="Nhập trọng lượng" />
+      <ErrorMessage :text="manualAuctionErrorState.weight" />
     </div>
+
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="brand"> Thương hiệu sản phẩm <span class="text-red-500">*</span> </label>
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="brand">
+        Thương hiệu sản phẩm <span class="text-red-500">*</span>
+      </label>
       <select
         v-model="formData.brandId"
-        required
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="brand">
         <option value="" disabled selected>Chọn thương hiệu</option>
         <option v-for="brand in brands" :key="brand.id" :value="brand.id">{{ brand.name }}</option>
       </select>
+      <ErrorMessage :text="manualAuctionErrorState.brand" />
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="category"> Loại sản phẩm <span class="text-red-500">*</span> </label>
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="category">
+        Loại sản phẩm <span class="text-red-500">*</span>
+      </label>
       <select
-        required
         v-model="formData.categoryId"
         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         id="category">
         <option value="" disabled selected>Chọn loại</option>
         <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
       </select>
+      <ErrorMessage :text="manualAuctionErrorState.category" />
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="category"> Hình ảnh <span class="text-red-500">*</span> </label>
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="category">
+        Hình ảnh <span class="text-red-500">*</span>
+      </label>
       <ListImage v-if="imgSrc.length > 0" :img-src="imgSrc" @deleted="handleImageDeleted" />
       <button
         @click="() => $refs.file.click()"
@@ -179,6 +234,7 @@ const resetFormData = () => {
         <span>Tải ảnh lên</span>
       </button>
       <input type="file" hidden v-on:change="handleFileUpload($event)" ref="file" />
+      <ErrorMessage :text="manualAuctionErrorState.image" />
     </div>
     <div class="flex items-center justify-between">
       <button
