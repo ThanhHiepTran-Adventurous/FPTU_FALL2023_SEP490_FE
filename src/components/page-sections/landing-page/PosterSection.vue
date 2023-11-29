f913fca86863fa832278710c63bf8330ebeae738
Truong Quang Tan <quangtan9bgm@gmail.com>
2023-11-29 13:29:18 +0700

    change poster order

--- src/components/page-sections/landing-page/PosterSection.vue
+++ src/components/page-sections/landing-page/PosterSection.vue
@@ -5,17 +5,17 @@ import 'vue3-carousel/dist/carousel.css'
 <template>
     <div class="bg-white my-2">
         <Carousel>
+            <Slide key="banner2">
+                <div class="container">
+                <img src="https://i.ibb.co/dKVJQfG/4324324324.png" alt="banner" class="w-full h-auto" />
+                </div>
+            </Slide>
             <Slide key="banner1">
                 <div class="container">
                 <img
                     src="https://i.ibb.co/2kVHKzN/370125377-764410905524764-4578443166790669082-n.png"
                     alt="banner"
                     class="w-full h-auto" />
-                </div>
-            </Slide>
-            <Slide key="banner2">
-                <div class="container">
-                <img src="https://i.ibb.co/dKVJQfG/4324324324.png" alt="banner" class="w-full h-auto" />
                 </div>
             </Slide>
 
