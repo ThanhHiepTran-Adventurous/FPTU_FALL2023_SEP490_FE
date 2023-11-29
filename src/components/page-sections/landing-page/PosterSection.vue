55f43c4e97574311ba985881bb358e3b6fb67ee2
Truong Quang Tan <quangtan9bgm@gmail.com>
2023-11-29 13:01:52 +0700

    poster section

--- src/components/page-sections/landing-page/PosterSection.vue
+++ src/components/page-sections/landing-page/PosterSection.vue
@@ -7,19 +7,18 @@ import 'vue3-carousel/dist/carousel.css'
         <Carousel>
             <Slide key="banner1">
                 <div class="container">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png" alt="banner" class="w-full h-[50vh]"/>
+                <img
+                    src="https://i.ibb.co/2kVHKzN/370125377-764410905524764-4578443166790669082-n.png"
+                    alt="banner"
+                    class="w-full h-auto" />
                 </div>
             </Slide>
             <Slide key="banner2">
                 <div class="container">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png" alt="banner" class="w-full h-[50vh]"/>
+                <img src="https://i.ibb.co/dKVJQfG/4324324324.png" alt="banner" class="w-full h-auto" />
                 </div>
             </Slide>
-            <Slide key="banner3">
-                <div class="container">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png" alt="banner" class="w-full h-[50vh]"/>
-                </div>
-            </Slide>
+
             <template #addons>
                 <Navigation />
             </template>
