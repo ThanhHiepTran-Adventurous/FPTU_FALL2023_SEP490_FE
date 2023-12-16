5bdc60e84335e134f9853c32d6932ed652aa677e
Truong Quang Tan <quangtan9bgm@gmail.com>
2023-12-16 13:14:25 +0700

    coding convention fix

--- src/views/admin/AdminReviewAuctions.vue
+++ src/views/admin/AdminReviewAuctions.vue
@@ -38,7 +38,7 @@ const getAllAuctions = async () => {
   try {
     const response = await adminService.getAllAuctions(1, 100, 'status:NEW')
     autionsList.value = response.data ? response.data : []
-    autionsList.value = autionsList.value.sort((a, b) => new Date(b.createAt).getTime() - new Date(a.createAt).getTime())
+    autionsList.value = autionsList.value.toSorted((a, b) => new Date(b.createAt).getTime() - new Date(a.createAt).getTime())
   } catch (e) {
     console.error(e)
   } finally {
