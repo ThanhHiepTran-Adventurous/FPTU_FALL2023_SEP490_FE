f0730b17d1124c21da32626a65c9380cbb9bff21
Truong Quang Tan <quangtan9bgm@gmail.com>
2023-12-16 22:29:04 +0700

    fix cate and responsive

--- src/components/page-sections/NewTopBar.vue
+++ src/components/page-sections/NewTopBar.vue
@@ -7,7 +7,7 @@ import TopBarItem from '@/components/page-sections/TopBarItem.vue'
       <router-link to="/auctions?category=9290b770-4263-11ee-b20e-9828a648e145"><TopBarItem text="Áo" icon="shirt" /></router-link>
       <router-link to="/auctions?category=c7a589c1-9b32-11ee-871a-8685dbe3cd15"><TopBarItem text="Quần" icon="pants" /></router-link>
       <router-link to="/auctions?category=c7a42652-9b32-11ee-871a-8685dbe3cd15"><TopBarItem text="Phụ kiện" icon="accessory" /></router-link>
-      <router-link to="/auctions?category=9290b770-4263-11ee-b20e-9828a648e145"><TopBarItem text="Giày" icon="shoes" /></router-link>
+      <router-link to="/auctions?category=c7a6bbda-9b32-11ee-871a-8685dbe3cd15"><TopBarItem text="Giày" icon="shoes" /></router-link>
       <router-link to="/auctions/outstanding"><TopBarItem text="Nổi bật" icon="topSeller" /></router-link>
     </div>
   </div>
